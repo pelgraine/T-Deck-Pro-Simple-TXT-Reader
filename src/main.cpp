2844a8a818653cdb2a45f5796081c9d4f80f816a
pelgraine <140762863+pelgraine@users.noreply.github.com>
2026-02-05 19:27:48 +1100

    fixed build date typo

--- src/main.cpp
+++ src/main.cpp
@@ -69,7 +69,7 @@ GxEPD2_BW<GxEPD2_310_GDEQ031T10, GxEPD2_310_GDEQ031T10::HEIGHT> display(
 
 // Version info
 #define VERSION "1.1.0"
-#define BUILD_DATE "Feb 2025"
+#define BUILD_DATE "Feb 2026"
 
 // Index file version - increment when format changes
 #define INDEX_VERSION 2
