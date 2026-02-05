9283ce75beb20b8942003e59527f6a734ad43b80
pelgraine <140762863+pelgraine@users.noreply.github.com>
2026-02-05 19:31:56 +1100

    updated firmware version info to be consistent with alpha status

--- src/main.cpp
+++ src/main.cpp
@@ -68,7 +68,7 @@ GxEPD2_BW<GxEPD2_310_GDEQ031T10, GxEPD2_310_GDEQ031T10::HEIGHT> display(
 // ============================================================================
 
 // Version info
-#define VERSION "1.1.0"
+#define VERSION "0.0.2"
 #define BUILD_DATE "Feb 2026"
 
 // Index file version - increment when format changes
