27eaabac48e22646177e57752332d05634c8ac75
pelgraine <140762863+pelgraine@users.noreply.github.com>
2026-02-05 19:48:26 +1100

    "Splash screen now working"

--- src/main.cpp
+++ src/main.cpp
@@ -68,7 +68,7 @@ GxEPD2_BW<GxEPD2_310_GDEQ031T10, GxEPD2_310_GDEQ031T10::HEIGHT> display(
 // ============================================================================
 
 // Version info
-#define VERSION "0.0.2"
+#define VERSION "0.0.3"
 #define BUILD_DATE "Feb 2026"
 
 // Index file version - increment when format changes
@@ -162,7 +162,7 @@ void setup() {
     initKeyboard();
     
     // Skip splash screen for now - focus on fixing navigation
-    // showSplashScreen();
+    showSplashScreen();
     
     listTextFiles();
     preIndexFiles();  // Pre-index files for fast opening
